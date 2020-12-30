7e0ff7ff446c8a0349c2c61af7135b75e9e9f822
Serg Yelagin <yelagins@gmail.com>
2020-12-30 14:22:25 +0200

    change: add env variables

--- src/functions/auth.js
+++ src/functions/auth.js
@@ -1,7 +1,7 @@
 import client from 'vortex-account-client-js'; 
 
 export const auth = new client.WebAuth({
-    clientID: 'democlientid',
-    agentDomain: 'localhost:3001',
-    redirectUri: 'http://localhost:3000/'
+    clientID:    process.env.REACT_APP_CLIENT_ID,
+    agentDomain: process.env.REACT_APP_AGENT_DOMAIN,
+    redirectUri: process.env.REACT_APP_REDIRECT_URI
 });
