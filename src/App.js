7c8b12aa108f284bca36a83a77f0d47f5c3d7212
Serg Yelagin <yelagins@gmail.com>
2021-05-27 19:36:28 +0200

    add: Button to demo shop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { observer } from 'mobx-react'
 import { Layout, Avatar, Button, Menu, Dropdown } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 
 import { Landing, Background } from './components'
 import { useStores } from './store/RootStore'
@@ -25,7 +25,12 @@ const App = observer(() => {
                 <Background/>
                 <Header>
                     <div className="header-components-wrapper">
-                        <h1>Demo Site</h1>
+                        <div className="left-nav">
+                            <h1>Demo Site</h1> 
+                            <Button type="primary" onClick={()=> {window.location.href = 'https://account-demo-shop.testnet.velas.com'}} size={'large'}><ShoppingCartOutlined /> Demo Shop</Button>
+                        </div>
+                      
+
                         <div className="header-actions"> 
                             { auth.authorization
                                 ? <Dropdown overlay={menu} trigger={['click']}>
@@ -34,7 +39,7 @@ const App = observer(() => {
                                         <span className="account-name">{auth.authorization.access_token_payload.sub.slice(0,4)}..{auth.authorization.access_token_payload.sub.substr(-4)}</span>
                                     </div>
                                   </Dropdown>
-                                : <Button type="primary" onClick={login} size={'large'}>Login</Button>
+                                : <Button type="primary" onClick={login} size={'large'}><UserOutlined/> Login</Button>
                             }
                         </div>
                     </div>
