9494ce3dad1c1e94c3d87839984b65a53d59cfa9
Serg <yelagins@gmail.com>
2022-07-02 20:55:02 +0300

    update: velas client

--- src/Mobile.js
+++ src/Mobile.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-
+import { vaclient_mobile }  from './functions/vaclient';
 
 const Mobile = () => {
+
+    const checkActiveSession = () => {
+        vaclient_mobile.handleRedirectCallback((a, b)=>{ console.log(a,b)});
+    };
+
+    useEffect(checkActiveSession, []);
+
     return <h2>...</h2>;
 };
 
